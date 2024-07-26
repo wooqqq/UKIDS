7e753902bdc453f00d9de14574d3c3477554f6c1
정재희 <wogml5890@gmail.com>
2024-07-26 15:57:48 +0900

    begin: 프로젝트 초기 설정

---
+++ UKIDS/src/main/java/com/modernfamily/ukids/UkidsApplication.java
@@ -0,0 +1,13 @@
+package com.modernfamily.ukids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UkidsApplication.class, args);
+	}
+
+}
---
+++ UKIDS/src/test/java/com/modernfamily/ukids/UkidsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modernfamily.ukids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkidsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
