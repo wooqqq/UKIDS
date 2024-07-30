3cad7db9883473d7853e4b3c75346ce1fbf75cbf
wooqqq <sge0225@naver.com>
2024-07-30 13:32:52 +0900

    comment: Letter 필요한 기능 주석 추가

--- UKIDS/src/main/java/com/modernfamily/ukids/domain/letter/controller/LetterController.java
+++ UKIDS/src/main/java/com/modernfamily/ukids/domain/letter/controller/LetterController.java
@@ -1,9 +1,11 @@
 package com.modernfamily.ukids.domain.letter.controller;
 
 import com.modernfamily.ukids.domain.letter.model.LetterService;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/letter")
 public class LetterController {
 
     private LetterService letterService;
@@ -11,4 +13,18 @@ public class LetterController {
     public LetterController (LetterService letterService) {
         this.letterService = letterService;
     }
+
+    // 편지 작성
+
+    // 요청 사용자 id가 받은 편지 리스트 조회
+
+    // 요청 사용자 id가 보낸 편지 리스트 조회
+
+    // 편지 내용 상세 조회
+
+    // 편지 삭제
+
+    // 타임캡슐 오픈하여 편지 상태(isOpen)를 true로 변경
+    // 프론트로부터 오픈 요청 받기 (편지 개수 보내기)
+
 }
