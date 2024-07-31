726fe404384f231cb87f44664a6039de05d22358
wooqqq <sge0225@naver.com>
2024-07-31 14:18:59 +0900

    refactor: tree 엔티티 수정

--- UKIDS/src/main/java/com/modernfamily/ukids/domain/tree/entity/Tree.java
+++ UKIDS/src/main/java/com/modernfamily/ukids/domain/tree/entity/Tree.java
@@ -4,27 +4,31 @@ import com.modernfamily.ukids.domain.family.entity.Family;
 import com.modernfamily.ukids.domain.letter.entity.Letter;
 import com.modernfamily.ukids.global.baseTimeEntity.BaseTimeEntity;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 
 import java.util.List;
 
 @Entity
+@NoArgsConstructor
 @Getter
 public class Tree extends BaseTimeEntity {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long treeId;
-
-    @OneToMany(mappedBy = "tree")
-    private List<Letter> letters;
 
     @Column(nullable = false)
     public Long exp = 0L;   // 경험치 default = 0
 
     @ColumnDefault("false")
-    @Column(columnDefinition = "TINYINT(1)")
+    @Column(columnDefinition = "TINYINT(1)", nullable = false)
     private boolean isComplete;
+
+    @OneToMany(mappedBy = "tree")
+    private List<Letter> letters;
 
     @ManyToOne
     @JoinColumn(name = "tree_type_id")
@@ -33,5 +37,13 @@ public class Tree extends BaseTimeEntity {
     @ManyToOne
     @JoinColumn(name = "family_id")
     private Family family;
+
+    @Builder
+    private Tree(Long exp, boolean isComplete, TreeType treeType, Family family) {
+        this.exp = exp;
+        this.isComplete = isComplete;
+        this.treeType = treeType;
+        this.family = family;
+    }
 
 }
