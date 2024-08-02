4e56a28fec1724390d9746a4c8d2bd7e2d057b65
김예현 <yh980827@naver.com>
2024-08-02 09:20:50 +0900

    chore: OpenVidu 2.3.0 -> 3.0.0 / livekit-client 도입 / 관련코드 수정

--- ukids/src/components/feature/videocall/VideoCall.tsx
+++ ukids/src/components/feature/videocall/VideoCall.tsx
@@ -1,211 +1,273 @@
-import React, { useState, useEffect, useCallback } from 'react';
 import {
-  OpenVidu,
-  Session as OVSession,
-  Publisher,
-  Subscriber,
-} from 'openvidu-browser';
-import axios, { AxiosError } from 'axios';
-import Form from './Form';
-import Session from './Session';
+  LocalVideoTrack,
+  LocalAudioTrack,
+  RemoteParticipant,
+  RemoteTrack,
+  RemoteTrackPublication,
+  Room,
+  RoomEvent,
+} from 'livekit-client';
+import './VideoCall.css';
+import { useState } from 'react';
+import VideoComponent from './VideoComponent';
+import AudioComponent from './AudioComponent';
+
+type TrackInfo = {
+  trackPublication: RemoteTrackPublication;
+  participantIdentity: string;
+};
+
+// 개발 환경을 위한 기본 URL 설정
+let APPLICATION_SERVER_URL = '';
+let LIVEKIT_URL = '';
+configureUrls();
+
+function configureUrls() {
+  // APPLICATION_SERVER_URL이 설정되지 않은 경우, 로컬 개발 환경의 기본 값을 사용
+  if (!APPLICATION_SERVER_URL) {
+    if (window.location.hostname === 'localhost') {
+      APPLICATION_SERVER_URL = 'http://localhost:6080/';
+    } else {
+      APPLICATION_SERVER_URL = 'https://' + window.location.hostname + ':6443/';
+    }
+  }
+
+  // LIVEKIT_URL이 설정되지 않은 경우, 로컬 개발 환경의 기본 값을 사용
+  if (!LIVEKIT_URL) {
+    if (window.location.hostname === 'localhost') {
+      LIVEKIT_URL = 'ws://localhost:7880/';
+    } else {
+      LIVEKIT_URL = 'wss://' + window.location.hostname + ':7443/';
+    }
+  }
+}
 
 function VideoCall() {
-  const [session, setSession] = useState<OVSession | ''>('');
-  const [sessionId, setSessionId] = useState<string>('');
-  const [subscribers, setSubscribers] = useState<
-    { subscriber: Subscriber; name: string }[]
-  >([]);
-  const [publisher, setPublisher] = useState<{
-    publisher: Publisher;
-    name: string;
-  } | null>(null);
-  const [OV, setOV] = useState<OpenVidu | null>(null);
-  const [isVideoEnabled, setIsVideoEnabled] = useState<boolean>(false);
-  const [isAudioEnabled, setIsAudioEnabled] = useState<boolean>(false);
-  const [userName, setUserName] = useState<string>('');
-  const OPENVIDU_SERVER_URL = `https://${window.location.hostname}:4443`;
-  const OPENVIDU_SERVER_SECRET = 'MY_SECRET';
+  const [room, setRoom] = useState<Room | undefined>(undefined); // 방 객체 상태 관리
+  const [localVideoTrack, setLocalVideoTrack] = useState<
+    LocalVideoTrack | undefined
+  >(undefined); // 로컬 비디오 트랙 상태 관리
+  const [localAudioTrack, setLocalAudioTrack] = useState<
+    LocalAudioTrack | undefined
+  >(undefined); // 로컬 오디오 트랙 상태 관리
+  const [remoteTracks, setRemoteTracks] = useState<TrackInfo[]>([]); // 원격 트랙 정보 상태 관리
 
-  console.log(window.location.hostname);
+  const [participantName, setParticipantName] = useState(
+    'Participant' + Math.floor(Math.random() * 100),
+  ); // 참가자 이름 상태 관리
+  const [roomName, setRoomName] = useState('Test Room'); // 방 이름 상태 관리
 
-  const leaveSession = useCallback(() => {
-    if (session) session.disconnect();
-    setOV(null);
-    setSession('');
-    setSessionId('');
-    setSubscribers([]);
-    setPublisher(null);
-    setIsVideoEnabled(false);
-    setIsAudioEnabled(false);
-  }, [session]);
+  const [isVideoEnabled, setIsVideoEnabled] = useState(true); // 비디오 활성화 상태 관리
+  const [isAudioEnabled, setIsAudioEnabled] = useState(true); // 오디오 활성화 상태 관리
 
-  const joinSession = () => {
-    const OVs = new OpenVidu();
-    setOV(OVs);
-    const newSession = OVs.initSession();
-    setSession(newSession);
+  // 방에 참여하는 함수
+  async function joinRoom() {
+    const room = new Room();
+    setRoom(room);
 
-    newSession.on('streamDestroyed', (event) => {
-      setSubscribers((prevSubscribers) =>
-        prevSubscribers.filter(
-          (subscriber) =>
-            subscriber.subscriber.stream.streamId !== event.stream.streamId,
-        ),
-      );
-    });
-
-    newSession.on('streamCreated', (event) => {
-      const newSubscriber = newSession.subscribe(event.stream, '');
-      const userData = JSON.parse(
-        event.stream.connection.data.split('%/%')[0],
-      ).clientData;
-      setSubscribers((prevSubscribers) => [
-        ...prevSubscribers,
-        { subscriber: newSubscriber, name: userData },
-      ]);
-    });
-  };
-
-  useEffect(() => {
-    window.addEventListener('beforeunload', leaveSession);
-    return () => {
-      window.removeEventListener('beforeunload', leaveSession);
-    };
-  }, [leaveSession]);
-
-  const sessionIdChangeHandler = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    setSessionId(event.target.value);
-  };
-
-  const userNameChangeHandler = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    setUserName(event.target.value);
-  };
-
-  useEffect(() => {
-    if (session === '' || !OV) return;
-
-    const createSession = async (sessionIds: string): Promise<string> => {
-      try {
-        const data = JSON.stringify({ customSessionId: sessionIds });
-        const response = await axios.post(
-          `${OPENVIDU_SERVER_URL}/openvidu/api/sessions`,
-          data,
+    // 새로운 트랙을 받을 때마다 실행
+    room.on(
+      RoomEvent.TrackSubscribed,
+      (
+        _track: RemoteTrack,
+        publication: RemoteTrackPublication,
+        participant: RemoteParticipant,
+      ) => {
+        setRemoteTracks((prev) => [
+          ...prev,
           {
-            headers: {
-              Authorization: `Basic ${btoa(
-                `OPENVIDUAPP:${OPENVIDU_SERVER_SECRET}`,
-              )}`,
-              'Content-Type': 'application/json',
-            },
+            trackPublication: publication,
+            participantIdentity: participant.identity,
           },
+        ]);
+      },
+    );
+
+    // 트랙이 제거될 때마다 실행
+    room.on(
+      RoomEvent.TrackUnsubscribed,
+      (_track: RemoteTrack, publication: RemoteTrackPublication) => {
+        setRemoteTracks((prev) =>
+          prev.filter(
+            (track) => track.trackPublication.trackSid !== publication.trackSid,
+          ),
         );
-        return (response.data as { id: string }).id;
-      } catch (error) {
-        const errorResponse = (error as AxiosError)?.response;
-        if (errorResponse?.status === 409) {
-          return sessionIds;
-        }
-        return '';
-      }
-    };
+      },
+    );
 
-    const createToken = (sessionIds: string): Promise<string> => {
-      return new Promise((resolve, reject) => {
-        const data = {};
-        axios
-          .post(
-            `${OPENVIDU_SERVER_URL}/openvidu/api/sessions/${sessionIds}/connection`,
-            data,
-            {
-              headers: {
-                Authorization: `Basic ${btoa(
-                  `OPENVIDUAPP:${OPENVIDU_SERVER_SECRET}`,
-                )}`,
-                'Content-Type': 'application/json',
-              },
-            },
-          )
-          .then((response) => {
-            resolve((response.data as { token: string }).token);
-          })
-          .catch((error) => reject(error));
-      });
-    };
+    try {
+      // 서버에서 토큰을 받아옴
+      const token = await getToken(roomName, participantName);
 
-    const getToken = async (): Promise<string> => {
-      try {
-        const sessionIds = await createSession(sessionId);
-        const token = await createToken(sessionIds);
-        return token;
-      } catch (error) {
-        throw new Error('Failed to get token.');
-      }
-    };
+      // LiveKit URL과 토큰을 사용하여 방에 연결
+      await room.connect(LIVEKIT_URL, token);
 
-    getToken()
-      .then((token) => {
-        session
-          .connect(token, { clientData: userName })
-          .then(() => {
-            const publishers = OV.initPublisher(undefined, {
-              audioSource: undefined,
-              videoSource: undefined,
-              publishAudio: false,
-              publishVideo: false,
-              mirror: false,
-            });
-            setPublisher({ publisher: publishers, name: '나' });
-            session.publish(publishers).catch(() => {});
-          })
-          .catch(() => {});
-      })
-      .catch(() => {});
-  }, [session, OV, sessionId, userName, OPENVIDU_SERVER_URL]);
+      // 카메라와 마이크를 활성화하여 퍼블리시
+      await room.localParticipant.enableCameraAndMicrophone();
 
+      // 로컬 비디오 및 오디오 트랙 설정
+      setLocalVideoTrack(
+        room.localParticipant.videoTrackPublications.values().next().value
+          .videoTrack,
+      );
+      setLocalAudioTrack(
+        room.localParticipant.audioTrackPublications.values().next().value
+          .audioTrack,
+      );
+    } catch (error) {
+      console.log(
+        '방에 연결하는 동안 오류가 발생했습니다:',
+        (error as Error).message,
+      );
+      await leaveRoom();
+    }
+  }
+
+  // 방을 떠나는 함수
+  async function leaveRoom() {
+    await room?.disconnect(); // 방에서 연결을 끊음
+
+    // 상태 초기화
+    setRoom(undefined);
+    setLocalVideoTrack(undefined);
+    setLocalAudioTrack(undefined);
+    setRemoteTracks([]);
+  }
+
+  // 서버에서 토큰을 받아오는 함수
+  async function getToken(roomName: string, participantName: string) {
+    const response = await fetch(APPLICATION_SERVER_URL + 'token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        roomName: roomName,
+        participantName: participantName,
+      }),
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+      throw new Error(`토큰을 가져오는 데 실패했습니다: ${error.errorMessage}`);
+    }
+
+    const data = await response.json();
+    return data.token;
+  }
+
+  // 비디오 토글 함수
   const toggleVideo = () => {
-    if (publisher) {
-      const newState = !isVideoEnabled;
-      publisher.publisher.publishVideo(newState);
-      setIsVideoEnabled(newState);
+    if (localVideoTrack) {
+      if (isVideoEnabled) {
+        localVideoTrack.mute(); // 비디오 비활성화
+      } else {
+        localVideoTrack.unmute(); // 비디오 활성화
+      }
+      setIsVideoEnabled(!isVideoEnabled);
     }
   };
 
+  // 오디오 토글 함수
   const toggleAudio = () => {
-    if (publisher) {
-      const newState = !isAudioEnabled;
-      publisher.publisher.publishAudio(newState);
-      setIsAudioEnabled(newState);
+    if (localAudioTrack) {
+      if (isAudioEnabled) {
+        localAudioTrack.mute(); // 오디오 비활성화
+      } else {
+        localAudioTrack.unmute(); // 오디오 활성화
+      }
+      setIsAudioEnabled(!isAudioEnabled);
     }
   };
 
   return (
-    <div>
-      <h1 className="text-2xl">가족통화</h1>
-      {session ? (
-        <>
-          <Session publisher={publisher} subscribers={subscribers} />
-          <button onClick={leaveSession}>돌아가기</button>
-          <button onClick={toggleVideo}>
-            {isVideoEnabled ? '비디오 끄기' : '비디오 켜기'}
-          </button>
-          <button onClick={toggleAudio}>
-            {isAudioEnabled ? '음소거' : '음소거 해제'}
-          </button>
-        </>
+    <>
+      {!room ? (
+        <div id="join">
+          <div id="join-dialog">
+            <h2>Join a Video Room</h2>
+            <form
+              onSubmit={(e) => {
+                joinRoom();
+                e.preventDefault();
+              }}
+            >
+              <div>
+                <label htmlFor="participant-name">참여자: </label>
+                <input
+                  id="participant-name"
+                  className="form-control"
+                  type="text"
+                  value={participantName}
+                  onChange={(e) => setParticipantName(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="room-name">방: </label>
+                <input
+                  id="room-name"
+                  className="form-control"
+                  type="text"
+                  value={roomName}
+                  onChange={(e) => setRoomName(e.target.value)}
+                  required
+                />
+              </div>
+              <button
+                className="bg-slate-800 text-white"
+                type="submit"
+                disabled={!roomName || !participantName}
+              >
+                참여하기
+              </button>
+            </form>
+          </div>
+        </div>
       ) : (
-        <Form
-          joinSession={joinSession}
-          sessionId={sessionId}
-          sessionIdChangeHandler={sessionIdChangeHandler}
-          userName={userName}
-          userNameChangeHandler={userNameChangeHandler}
-        />
+        <div id="room">
+          <div id="room-header">
+            <h2 id="room-title">{roomName}</h2>
+            <button
+              className="btn btn-danger"
+              id="leave-room-button"
+              onClick={leaveRoom}
+            >
+              종료하기
+            </button>
+            <button className="btn btn-secondary" onClick={toggleVideo}>
+              {isVideoEnabled ? '비디오 끄기' : '비디오 켜기'}
+            </button>
+            <button className="btn btn-secondary" onClick={toggleAudio}>
+              {isAudioEnabled ? '음소거' : '음소거 해제'}
+            </button>
+          </div>
+          <div id="layout-container">
+            {localVideoTrack && (
+              <VideoComponent
+                track={localVideoTrack}
+                participantIdentity={participantName}
+                local={true}
+              />
+            )}
+            {remoteTracks.map((remoteTrack) =>
+              remoteTrack.trackPublication.kind === 'video' ? (
+                <VideoComponent
+                  key={remoteTrack.trackPublication.trackSid}
+                  track={remoteTrack.trackPublication.videoTrack!}
+                  participantIdentity={remoteTrack.participantIdentity}
+                />
+              ) : (
+                <AudioComponent
+                  key={remoteTrack.trackPublication.trackSid}
+                  track={remoteTrack.trackPublication.audioTrack!}
+                />
+              ),
+            )}
+          </div>
+        </div>
       )}
-    </div>
+    </>
   );
 }
 
