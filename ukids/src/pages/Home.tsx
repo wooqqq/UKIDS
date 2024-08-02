b750e00cf41db965877162ec9e65db79cb6b8998
unknown <ski03259@naver.com>
2024-08-02 17:31:13 +0900

    feat: 홈화면을 수정

--- ukids/src/pages/Home.tsx
+++ ukids/src/pages/Home.tsx
@@ -1,38 +1,37 @@
+import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import FeatureBox from '../components/common/FeatureBox';
+import Sidebar from '../components/common/Sidebar';
 
 const Home = () => {
-  // const API_KEY = import.meta.env.VITE_API_KEY;
-
   return (
     <>
       <Header />
-      <div className="main-layout">
+      <div style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'start',
+        padding: '20px'
+      }}>
         {/* 메뉴바 */}
+        <div style={{
+          width: '200px',
+          paddingRight: '20px'
+        }}>
+          <Sidebar />
+        </div>
 
         {/* 콘텐츠 */}
-        <FeatureBox />
+        <div style={{
+          flexGrow: 1,
+          maxWidth: 'none', // 기본 최대 너비 제한을 없애기
+          padding: '0 20px' // 양쪽에 패딩 추가하여 콘텐츠 간격 유지
+        }}>
+          <FeatureBox />
+        </div>
       </div>
       <Footer />
-
-      {/* <GrayButton name="가족대화방" path="/chat-room" />
-      <GrayButton name="마이페이지" path="/user" />
-      <GrayButton name="로그아웃" path="/" />
-      <BlueButton name="편지쓰기" path="/letters/write" />
-      <RedButton name="가족해체" path="/info" />
-      <BlueButton name="연결하기" path="/chat/call" />
-      <WhiteButton name="삭제" path="/albums/list" />
-      <WhiteButton name="목록" path="/albums/list" />
-      <CharacterTag character="엄마" />
-      <CharacterTag character="아빠" />
-      <CharacterTag character="딸" />
-      <CharacterTag character="아들" />
-      <CharacterTag character="할머니" />
-      <CharacterTag character="할아버지" />
-      <CharacterTag character="손자" />
-      <CharacterTag character="손녀" />
-      <ProfileList /> */}
     </>
   );
 };
