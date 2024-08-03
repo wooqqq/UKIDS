e16e10ffc57ffe3cc4a4956cd036c04f97cc062d
hhsssu <hhssuu14@naver.com>
2024-08-03 13:16:22 +0900

    feat: 달력 기능 테스트

--- ukids/src/components/feature/calendar/CalendarBox.tsx
+++ ukids/src/components/feature/calendar/CalendarBox.tsx
@@ -6,10 +6,29 @@ export default function Calendar() {
     <FullCalendar
       plugins={[dayGridPlugin]}
       initialView="dayGridMonth"
-      weekends={false}
+      weekends={true}
+      fixedWeekCount={false}
       events={[
-        { title: 'event 1', date: '2019-04-01' },
-        { title: 'event 2', date: '2019-04-02' },
+        {
+          title: '할머니할아버지와 제주도 여행!',
+          // date: '2024-08-01',
+          allDay: true,
+          start: '2024-08-01', // 시작 일자
+          end: '2024-08-05', // 끝나는 일자 -> 표시는 전날까지 됨
+          // backgroundColor: '#FFBF33', // 배경색
+          // borderColor: '#FFBF33', // 테두리색
+          color: '#FFBF33', // 배경, 테두리색 모두
+          textColor: '#fff', // 글자 색상 (우리는 흰색 고정)
+        },
+        {
+          title: '딸기 생일🍓',
+          // date: '2024-08-01',
+          allDay: true,
+          start: '2024-08-08', // 시작 일자
+          // end: '2024-08-08', // 끝나는 일자 -> 표시는 전날까지 됨
+          color: '#F6AEEF',
+          textColor: '#fff',
+        },
       ]}
     />
   );
