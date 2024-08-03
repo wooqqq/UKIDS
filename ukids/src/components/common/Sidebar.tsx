4884ee909b325771041de7e963211bd0622fddda
hhsssu <hhssuu14@naver.com>
2024-08-03 16:02:46 +0900

    feat: 사이드바 페이지 링크 연결

--- ukids/src/components/common/Sidebar.tsx
+++ ukids/src/components/common/Sidebar.tsx
@@ -1,11 +1,19 @@
+import { Link } from 'react-router-dom';
+
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div className="cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110">
+      <Link
+        to={'/albums'}
+        className="cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110"
+      >
         <img width="97" height="91" src="/assets/album.png" alt="사진 앨범" />
         <div className="sidebar-text">사진 앨범</div>
-      </div>
-      <div className="cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110">
+      </Link>
+      <Link
+        to={'/paintdiary'}
+        className="cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110"
+      >
         <img
           width="71"
           height="97"
@@ -13,19 +21,28 @@ const Sidebar = () => {
           alt="그림 일기"
         />
         <div className="sidebar-text">그림 일기</div>
-      </div>
-      <div className="cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110">
+      </Link>
+      <Link
+        to={'/growthdiary'}
+        className="cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110"
+      >
         <img width="70" height="95" src="/assets/growth.png" alt="성장 일지" />
         <div className="sidebar-text">성장 일지</div>
-      </div>
-      <div className="cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110">
+      </Link>
+      <Link
+        to={'/chat'}
+        className="cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110"
+      >
         <img width="73" height="78" src="/assets/chat.png" alt="대화방" />
         <div className="sidebar-text">대화방</div>
-      </div>
-      <div className="cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110">
+      </Link>
+      <Link
+        to={'/game'}
+        className="cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110"
+      >
         <img width="68" height="99" src="/assets/game.png" alt="게임" />
         <div className="sidebar-text">게임</div>
-      </div>
+      </Link>
     </div>
   );
 };
