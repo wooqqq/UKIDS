68a406b9504e8580fd4208397e3688fa08a1cb6e
wooqqq <sge0225@naver.com>
2024-08-07 02:17:47 +0900

    remove: 필요없는 파일 및 주석 제거

--- UKIDS/src/main/java/com/modernfamily/ukids/global/jwt/JWTFilter.java
+++ UKIDS/src/main/java/com/modernfamily/ukids/global/jwt/JWTFilter.java
@@ -21,12 +21,6 @@ public class JWTFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-//        String path = request.getRequestURI();
-//        if (path.startsWith("/chat/room")) {
-//            filterChain.doFilter(request, response);
-//            return;
-//        }
-
         String authorization = request.getHeader("Authorization");
 
         if(authorization == null || !authorization.startsWith("Bearer ")) {
