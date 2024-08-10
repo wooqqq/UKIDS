202fbc235f35cb66306879b37df4a0602dc2b220
정재희 <wogml5890@gmail.com>
2024-08-10 15:28:07 +0900

    feat: 퀴즈 게임 결과 반환 컨트롤러 작성

--- UKIDS/src/main/java/com/modernfamily/ukids/domain/game/quizResult/controller/QuizResultController.java
+++ UKIDS/src/main/java/com/modernfamily/ukids/domain/game/quizResult/controller/QuizResultController.java
@@ -1,6 +1,7 @@
 package com.modernfamily.ukids.domain.game.quizResult.controller;
 
 import com.modernfamily.ukids.domain.game.quizResult.model.service.QuizResultService;
+import com.modernfamily.ukids.global.util.HttpMethodCode;
 import com.modernfamily.ukids.global.util.HttpResponseUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +23,7 @@ public class QuizResultController {
     @GetMapping
     public ResponseEntity<Map<String, Object>> getQuizResult(@RequestParam(value = "size", defaultValue = "5") int size,
                                                              @RequestParam(value = "page", defaultValue = "1") int page) {
-
+        return responseUtil.createResponse(HttpMethodCode.GET, quizResultService.getQuizResults(size, page));
     }
 
 }
