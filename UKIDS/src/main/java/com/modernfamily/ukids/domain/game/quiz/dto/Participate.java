dbae465584b189ff0e09c4d152acd8e3d68b966f
정재희 <wogml5890@gmail.com>
2024-08-11 16:13:54 +0900

    docs: quiz response dto 수정

--- UKIDS/src/main/java/com/modernfamily/ukids/domain/game/quiz/dto/Participate.java
+++ UKIDS/src/main/java/com/modernfamily/ukids/domain/game/quiz/dto/Participate.java
@@ -1,23 +1,28 @@
 package com.modernfamily.ukids.domain.game.quiz.dto;
 
+import com.modernfamily.ukids.domain.familyMember.entity.FamilyRole;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class Participate {
+    private String userName;
+    private FamilyRole role;
     private long maxQuestion;
     private long hit;
     private boolean isReady;
 
-    private Participate(long maxQuestion, long hit, boolean isReady) {
+    private Participate(String userName, FamilyRole role, long maxQuestion, long hit, boolean isReady) {
+        this.userName = userName;
+        this.role = role;
         this.maxQuestion = maxQuestion;
         this.hit = hit;
         this.isReady = isReady;
     }
 
-    public static Participate createParticipate(long maxQuestion) {
-        return new Participate(maxQuestion, 0, false);
+    public static Participate createParticipate(String userName, FamilyRole role, long maxQuestion) {
+        return new Participate(userName, role, maxQuestion, 0, false);
     }
 
     public void hittingAnswer(){
