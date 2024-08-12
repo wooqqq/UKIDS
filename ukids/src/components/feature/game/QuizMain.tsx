ca6c5286f6b00dc73493b2e2b6d9bca394fe8252
김예현 <yh980827@naver.com>
2024-08-13 05:28:54 +0900

    design: 퀴즈, 콜마넴 Main CSS 수정 및 GamePageHeader에서 이전버튼 컴포넌트 분리

--- ukids/src/components/feature/game/QuizMain.tsx
+++ ukids/src/components/feature/game/QuizMain.tsx
@@ -1,4 +1,6 @@
 import { useNavigate } from 'react-router-dom';
+import GameBackButton from './GameBackButton';
+import gameExplain from '@/assets/game_explain.png';
 import './gamepart.css';
 
 const QuizMain = () => {
@@ -17,18 +19,43 @@ const QuizMain = () => {
     nav('history');
   };
 
+  // hover:border-solid border-8 border-[#FFBF33]
+
   return (
     <>
-      <div className="flex justify-evenly items-center w-full">
-        <button className="quiz-select-box" onClick={onClickStart}>
-          퀴즈 시작
-        </button>
-        <button className="quiz-select-box" onClick={onClickQnAList}>
-          질문 목록
-        </button>
-        <button className="quiz-select-box" onClick={onClickHistory}>
-          결과 기록
-        </button>
+      <div className="w-[80%] flex flex-col justify-between">
+        {/* 빈 div */}
+        <div className="h-[1rem]"></div>
+
+        {/* 윗 부분 */}
+        <div className="flex flex-row justify-between">
+          <GameBackButton path={'/game'} />
+          <img src={gameExplain} alt="설명" className="" />
+        </div>
+
+        {/* 본 영역 */}
+        <div className="flex justify-between items-center w-full px-4">
+          <button
+            className="quiz-select-box game-font quiz-font-color"
+            onClick={onClickStart}
+          >
+            퀴즈 시작
+          </button>
+          <button
+            className="quiz-select-box game-font quiz-font-color"
+            onClick={onClickQnAList}
+          >
+            질문 목록
+          </button>
+          <button
+            className="quiz-select-box game-font quiz-font-color"
+            onClick={onClickHistory}
+          >
+            결과 기록
+          </button>
+        </div>
+        {/* 빈 div */}
+        <div className="h-[8rem]"></div>
       </div>
     </>
   );
