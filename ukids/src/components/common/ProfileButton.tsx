63acf9737a66c149d6398ca1d9973dace79513f6
wooqqq <sge0225@naver.com>
2024-08-12 14:14:56 +0900

    feat: 가족 선택 시 가족 ID에 따라 나무 변경

--- ukids/src/components/common/ProfileButton.tsx
+++ ukids/src/components/common/ProfileButton.tsx
@@ -55,6 +55,8 @@ const ProfileButton = ({ name }: UserProps) => {
 
   // 가족방을 클릭했을 때
   const handleFamilyClick = (familyId: number) => {
+    window.location.reload();
+
     const selectedFamily = family.find(
       (family) => family.familyId === familyId,
     );
--- ukids/src/stores/treeStore.ts
+++ ukids/src/stores/treeStore.ts
@@ -2,51 +2,42 @@ import { create } from 'zustand';
 import api from '../util/api';
 
 interface TreeState {
+  familyId: number | null;
   treeData: any | null;
   fetchTreeData: (familyId: number) => Promise<void>;
-  updateTreeExp: (familyId: number, point: number) => Promise<void>;
+  updateTreeExp: (point: number) => Promise<void>;
   error: string | null;
+  setFamilyId: (familyId: number) => void;
 }
 
-export const useTreeStore = create<TreeState>((set) => {
-  const getSelectedFamilyId = () => {
-    const familyId = localStorage.getItem('selectedFamilyId');
-    return familyId ? parseInt(familyId, 10) : null;
-  };
-
-  const initializeTreeData = async () => {
-    const familyId = getSelectedFamilyId();
+export const useTreeStore = create<TreeState>((set, get) => ({
+  familyId: parseInt(localStorage.getItem('selectedFamilyId') || '0', 10),
+  treeData: null,
+  error: null,
+  fetchTreeData: async (familyId: number) => {
     if (familyId !== null) {
-      await fetchTreeData(familyId);
+      try {
+        const response = await api.get(`/tree/${familyId}`);
+        set({ treeData: response.data });
+      } catch (error: any) {
+        set({ error: error.message });
+      }
     }
-  };
-
-  const fetchTreeData = async (familyId: number) => {
-    try {
-      const response = await api.get(`/tree/${familyId}`);
-      set({ treeData: response.data });
-    } catch (error: any) {
-      set({ error: error.message });
+  },
+  updateTreeExp: async (point: number) => {
+    const { familyId } = get();
+    if (familyId !== null) {
+      try {
+        await api.put(`/tree`, { familyId, point });
+        const response = await api.get(`/tree/${familyId}`);
+        set({ treeData: response.data });
+      } catch (error: any) {
+        set({ error: error.message });
+      }
     }
-  };
-
-  const updateTreeExp = async (familyId: number, point: number) => {
-    try {
-      await api.put(`/tree`, { familyId, point });
-
-      const response = await api.get(`/tree/${familyId}`);
-      set({ treeData: response.data });
-    } catch (error: any) {
-      set({ error: error.message });
-    }
-  };
-
-  initializeTreeData();
-
-  return {
-    treeData: null,
-    fetchTreeData,
-    updateTreeExp,
-    error: null,
-  };
-});
+  },
+  setFamilyId: (familyId: number) => {
+    localStorage.setItem('selectedFamilyId', familyId.toString());
+    set({ familyId });
+  },
+}));
