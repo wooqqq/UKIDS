9ebb9680042e8a2f246f7fa6df60bc0708879d50
김예현 <yh980827@naver.com>
2024-08-15 21:58:24 +0900

    fix: tanstackquery develop 모드 삭제

--- ukids/src/main.tsx
+++ ukids/src/main.tsx
@@ -4,7 +4,6 @@ import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -13,7 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </BrowserRouter>,
   // </React.StrictMode>
